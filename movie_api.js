4b0cf91b5e1c0971f4083aa5d7c035f99be50af7
rony <rony49883@gmail.com>
2021-08-31 12:21:45 +0600

    movie list from api

--- movie_api.js
+++ movie_api.js
@@ -1,1 +1,8 @@
-console.log("test")
+//get top rated movie list api
+function get_top_ratedMovie_api() {
+
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=cfb3cf255ce240392713b406e60e0243')
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+get_top_ratedMovie_api();
